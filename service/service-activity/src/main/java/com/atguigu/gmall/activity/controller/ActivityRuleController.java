87348ddef586de769fb4e099bab461fdb042248c
憨憨耿鬼 <1062215549@qq.com>
2022-08-27 03:03:42 +0800

    活动规则接口

---
+++ service/service-activity/src/main/java/com/atguigu/gmall/activity/controller/ActivityRuleController.java
@@ -0,0 +1,11 @@
+package com.atguigu.gmall.activity.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/*
+    活动规则控制器
+ */
+@RestController
+@RequestMapping("/admin/")
+public class ActivityRuleController {
+}
